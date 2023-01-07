f856dd64d510ef11201d727683b92510887569b8
tluwaniMS <tluwani2020@gmail.com>
2023-01-07 12:56:23 +0200

    adding-first-phase-of-array-exercises

---
+++ exercises/array-exercises.js
@@ -0,0 +1,40 @@
+// Exercise 1.
+
+// 1.1. Return the element/value 2 from the array.
+const returnElementTwoFromArray = () => {
+  const array = [1, 2, 3, 4];
+};
+
+// 1.2. Return the element/value 4 from the array.
+const returnElementFourFromArray = () => {
+  const array = [1, 2, 3, 4];
+};
+
+// 1.3. Return the size of the array.
+const returnArraySize = () => {
+  const array = [1, 2, 3, 4];
+};
+
+// 1.4. Add the number five to the array.
+const addTheNumberFiveToTheArray = () => {
+  const array = [1, 2, 3, 4];
+};
+
+// Exercise 2.
+
+const exerciseTwoArray = [
+  [2, 4, 17, 19],
+  [6, 8, 9],
+  [1, 3]
+];
+
+// Exercise 3.
+
+const exerciseThreeArray = [
+  [1, 3],
+  [
+    [2, 4],
+    [7, 9, 11],
+    [6, 8]
+  ]
+];
