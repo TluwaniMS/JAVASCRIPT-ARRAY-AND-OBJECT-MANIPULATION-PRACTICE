22a357b56d75bdc060a67f5a988c79d0e5c46c91
tluwaniMS <tluwani2020@gmail.com>
2023-01-07 20:50:26 +0200

    exporting-exerciuse-modules

--- exercises/array-exercises.js
+++ exercises/array-exercises.js
@@ -1,39 +1,39 @@
 // Exercise 1.✍
 
 // 1.1. Return the element/value 2 from the array.⚡
-// Expected answer ✅
+// Expected answer 2 ✅
 
-const returnElementTwoFromArray = () => {
+module.exports.returnElementTwoFromArray = () => {
   const array = [1, 2, 3, 4];
 };
 
 // 1.2. Return the element/value 4 from the array.⚡
-// Expected answer ✅
+// Expected answer 4 ✅
 
-const returnElementFourFromArray = () => {
+module.exports.returnElementFourFromArray = () => {
   const array = [1, 2, 3, 4];
 };
 
 // 1.3. Return the size of the array.⚡
-// Expected answer ✅
+// Expected answer 4 ✅
 
-const returnArraySize = () => {
+module.exports.returnArraySize = () => {
   const array = [1, 2, 3, 4];
 };
 
 // 1.4. Add the number five to the array.⚡
-// Expected answer ✅
+// Expected answer [1, 2, 3, 4, 5] ✅
 
-const addTheNumberFiveToTheArray = () => {
+module.exports.addTheNumberFiveToTheArray = () => {
   const array = [1, 2, 3, 4];
 };
 
 // Exercise 2.✍
 
 // 2.1 Return the array size of the second element in the main array.⚡
-// Expected answer ✅
+// Expected answer 3 ✅
 
-const returnArraySizeOfSecondElementInMainArray = () => {
+module.exports.returnArraySizeOfSecondElementInMainArray = () => {
   const array = [
     [2, 4, 17, 19],
     [6, 8, 9],
@@ -41,10 +41,10 @@ const returnArraySizeOfSecondElementInMainArray = () => {
   ];
 };
 
-// 2.2 Return the array element containing numbers 6 and 8.⚡
-// Expected answer ✅
+// 2.2 Return the array element containing numbers 6, 8 and 9.⚡
+// Expected answer [6, 8, 9] ✅
 
-const returnTheArrayElementContainingNumbersSixAndEightFromArray = () => {
+module.exports.returnTheArrayElementContainingNumbersSixEightAndNineFromArray = () => {
   const array = [
     [2, 4, 17, 19],
     [6, 8, 9],
@@ -53,9 +53,9 @@ const returnTheArrayElementContainingNumbersSixAndEightFromArray = () => {
 };
 
 // 2.3 Return the element/value 3 from the array.⚡
-// Expected answer ✅
+// Expected answer 3 ✅
 
-const returnElementThreeFromArray = () => {
+module.exports.returnElementThreeFromArray = () => {
   const array = [
     [2, 4, 17, 19],
     [6, 8, 9],
@@ -64,9 +64,9 @@ const returnElementThreeFromArray = () => {
 };
 
 // 2.4 Add an array with numbers 7 and 8 to the main array.⚡
-// Expected answer ✅
+// Expected answer [[2, 4, 17, 19],[6, 8, 9],[1, 3],[7, 8]] ✅
 
-const addAnArrayToTheMainArrayWithNumberSevenAndEight = () => {
+module.exports.addAnArrayToTheMainArrayWithNumberSevenAndEight = () => {
   const array = [
     [2, 4, 17, 19],
     [6, 8, 9],
@@ -75,9 +75,9 @@ const addAnArrayToTheMainArrayWithNumberSevenAndEight = () => {
 };
 
 // 2.5 Return the array size of the first array element in the main array.⚡
-// Expected answer ✅
+// Expected answer 4 ✅
 
-const returnTheArraySizeOfTheFirstArrayElementInTheMainArray = () => {
+module.exports.returnTheArraySizeOfTheFirstArrayElementInTheMainArray = () => {
   const array = [
     [2, 4, 17, 19],
     [6, 8, 9],
@@ -88,9 +88,9 @@ const returnTheArraySizeOfTheFirstArrayElementInTheMainArray = () => {
 // Exercise 3.✍
 
 // 3.1 Return the array containing numbers 6 and 8⚡
-// Expected answer ✅
+// Expected answer [6, 8] ✅
 
-const returnArrayContainingSixAndEight = () => {
+module.exports.returnArrayContainingSixAndEight = () => {
   const array = [
     [1, 3],
     [
@@ -102,9 +102,9 @@ const returnArrayContainingSixAndEight = () => {
 };
 
 // 3.2 Return the element/value 4 from the array⚡
-// Expected answer ✅
+// Expected answer 4 ✅
 
-const returnValueFourFromArray = () => {
+module.exports.returnValueFourFromArray = () => {
   const array = [
     [1, 3],
     [
@@ -116,9 +116,9 @@ const returnValueFourFromArray = () => {
 };
 
 // 3.3 Add an array with elements/values 9,12 and 15 in the array containing the arrays that contain 2 and 4, and 6 and 8.⚡
-// Expected answer ✅
+// Expected answer [[1, 3],[[2, 4],[7, 9, 11],[6, 8],[9, 12, 15]]] ✅
 
-const addAnArrayWithElementsNineTwelveAndFifteen = () => {
+module.exports.addAnArrayWithElementsNineTwelveAndFifteen = () => {
   const array = [
     [1, 3],
     [
@@ -130,9 +130,9 @@ const addAnArrayWithElementsNineTwelveAndFifteen = () => {
 };
 
 // 3.4 Return the array size of the second array in the second position of the main array.⚡
-// Expected answer ✅
+// Expected answer 3 ✅
 
-const returnArraySizeOfTheSecondArrayInTheSecondPosition = () => {
+module.exports.returnArraySizeOfTheSecondArrayInTheSecondPosition = () => {
   const array = [
     [1, 3],
     [
