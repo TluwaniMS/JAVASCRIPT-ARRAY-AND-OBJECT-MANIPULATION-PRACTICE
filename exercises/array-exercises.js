8904d3001577d9a4685c864ef2bdc839f159312f
tluwaniMS <tluwani2020@gmail.com>
2023-01-07 20:22:56 +0200

    adding-comment-emojis

--- exercises/array-exercises.js
+++ exercises/array-exercises.js
@@ -1,40 +1,144 @@
-// Exercise 1.
+// Exercise 1.✍
 
-// 1.1. Return the element/value 2 from the array.
+// 1.1. Return the element/value 2 from the array.⚡
+// Expected answer ✅
+
 const returnElementTwoFromArray = () => {
   const array = [1, 2, 3, 4];
 };
 
-// 1.2. Return the element/value 4 from the array.
+// 1.2. Return the element/value 4 from the array.⚡
+// Expected answer ✅
+
 const returnElementFourFromArray = () => {
   const array = [1, 2, 3, 4];
 };
 
-// 1.3. Return the size of the array.
+// 1.3. Return the size of the array.⚡
+// Expected answer ✅
+
 const returnArraySize = () => {
   const array = [1, 2, 3, 4];
 };
 
-// 1.4. Add the number five to the array.
+// 1.4. Add the number five to the array.⚡
+// Expected answer ✅
+
 const addTheNumberFiveToTheArray = () => {
   const array = [1, 2, 3, 4];
 };
 
-// Exercise 2.
+// Exercise 2.✍
 
-const exerciseTwoArray = [
-  [2, 4, 17, 19],
-  [6, 8, 9],
-  [1, 3]
-];
+// 2.1 Return the array size of the second element in the main array.⚡
+// Expected answer ✅
 
-// Exercise 3.
+const returnArraySizeOfSecondElementInMainArray = () => {
+  const array = [
+    [2, 4, 17, 19],
+    [6, 8, 9],
+    [1, 3]
+  ];
+};
 
-const exerciseThreeArray = [
-  [1, 3],
-  [
-    [2, 4],
-    [7, 9, 11],
-    [6, 8]
-  ]
-];
+// 2.2 Return the array element containing numbers 6 and 8.⚡
+// Expected answer ✅
+
+const returnTheArrayElementContainingNumbersSixAndEightFromArray = () => {
+  const array = [
+    [2, 4, 17, 19],
+    [6, 8, 9],
+    [1, 3]
+  ];
+};
+
+// 2.3 Return the element/value 3 from the array.⚡
+// Expected answer ✅
+
+const returnElementThreeFromArray = () => {
+  const array = [
+    [2, 4, 17, 19],
+    [6, 8, 9],
+    [1, 3]
+  ];
+};
+
+// 2.4 Add an array with numbers 7 and 8 to the main array.⚡
+// Expected answer ✅
+
+const addAnArrayToTheMainArrayWithNumberSevenAndEight = () => {
+  const array = [
+    [2, 4, 17, 19],
+    [6, 8, 9],
+    [1, 3]
+  ];
+};
+
+// 2.5 Return the array size of the first array element in the main array.⚡
+// Expected answer ✅
+
+const returnTheArraySizeOfTheFirstArrayElementInTheMainArray = () => {
+  const array = [
+    [2, 4, 17, 19],
+    [6, 8, 9],
+    [1, 3]
+  ];
+};
+
+// Exercise 3.✍
+
+// 3.1 Return the array containing numbers 6 and 8⚡
+// Expected answer ✅
+
+const returnArrayContainingSixAndEight = () => {
+  const array = [
+    [1, 3],
+    [
+      [2, 4],
+      [7, 9, 11],
+      [6, 8]
+    ]
+  ];
+};
+
+// 3.2 Return the element/value 4 from the array⚡
+// Expected answer ✅
+
+const returnValueFourFromArray = () => {
+  const array = [
+    [1, 3],
+    [
+      [2, 4],
+      [7, 9, 11],
+      [6, 8]
+    ]
+  ];
+};
+
+// 3.3 Add an array with elements/values 9,12 and 15 in the array containing the arrays that contain 2 and 4, and 6 and 8.⚡
+// Expected answer ✅
+
+const addAnArrayWithElementsNineTwelveAndFifteen = () => {
+  const array = [
+    [1, 3],
+    [
+      [2, 4],
+      [7, 9, 11],
+      [6, 8]
+    ]
+  ];
+};
+
+// 3.4 Return the array size of the second array in the second position of the main array.⚡
+// Expected answer ✅
+
+const returnArraySizeOfTheSecondArrayInTheSecondPosition = () => {
+  const array = [
+    [1, 3],
+    [
+      [2, 4],
+      [7, 9, 11],
+      [6, 8]
+    ]
+  ];
+};
