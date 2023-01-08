1db16da4ba7dd0e0c8a7a0893bc2c3a90c928e96
tluwaniMS <tluwani2020@gmail.com>
2023-01-08 02:29:33 +0200

    first-phase-of-adding-tests

---
+++ tests/array-exercises.test.js
@@ -0,0 +1,59 @@
+describe("Tests for array exercises.", () => {
+  describe("Exercise 1 tests. (Array Exercises)", () => {
+    test("1.1 Testing solution for returning element/value 2", () => {
+      const {} = require("../exercises/array-exercises");
+    });
+
+    test("1.2 Testing solution for returning element/value 4", () => {
+      const {} = require("../exercises/array-exercises");
+    });
+
+    test("1.3 Testing the solution to return the array size", () => {
+      const {} = require("../exercises/array-exercises");
+    });
+
+    test("1.4 Testing the solution to add number 5 to the array", () => {
+      const {} = require("../exercises/array-exercises");
+    });
+  });
+
+  describe("Exercise 2 tests. (Array Exercises)", () => {
+    test("2.1 Testing the solution to check the array size of the second element in the main array.", () => {
+      const {} = require("../exercises/array-exercises");
+    });
+
+    test("2.2 Testing the solution to return the array element containing numbers 6, 8 and 9.", () => {
+      const {} = require("../exercises/array-exercises");
+    });
+
+    test("2.3 Testing the solution to return the element/value 3 from the array.", () => {
+      const {} = require("../exercises/array-exercises");
+    });
+
+    test("2.4 Testing the solution to add an array with numbers 7 and 8 to the main array.", () => {
+      const {} = require("../exercises/array-exercises");
+    });
+
+    test("2.5 Testing the solution to return the array size of the first array element in the main array.", () => {
+      const {} = require("../exercises/array-exercises");
+    });
+  });
+
+  describe("Exercise 3 tests. (Array Exercises)", () => {
+    test("3.1 Testing the solution to return the array containing numbers 6 and 8", () => {
+      const {} = require("../exercises/array-exercises");
+    });
+
+    test("3.2 Testing the solution to return the element/value 4 from the array", () => {
+      const {} = require("../exercises/array-exercises");
+    });
+
+    test("3.3 Testing the solution to add an array with elements/values 9,12 and 15 in the array containing the arrays that contain 2 and 4, and 6 and 8.", () => {
+      const {} = require("../exercises/array-exercises");
+    });
+
+    test("3.4 Testing the solution to return the array size of the second array in the second position of the main array.", () => {
+      const {} = require("../exercises/array-exercises");
+    });
+  });
+});
