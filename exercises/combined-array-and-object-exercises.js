d6936611148e29f9b4579b8873bdce4b38622edd
tluwaniMS <tluwani2020@gmail.com>
2023-01-10 03:02:10 +0200

    adding-the-first-phase-combined-array-and-object-exercises

---
+++ exercises/combined-array-and-object-exercises.js
@@ -0,0 +1,26 @@
+// Exercise 1.✍
+
+// 1.1. Return the persons lucky numbers.⚡
+// Expected answer  ✅
+
+// 1.2. Return the persons second listed lucky numbers.⚡
+// Expected answer  ✅
+
+// 1.3. Add the number 5 to the persons lucky numbers.⚡
+// Expected answer  ✅
+
+// Exercise 2.✍
+
+// 2.1. Return the total sports the person is interested in.⚡
+// Expected answer  ✅
+
+// 2.2. Add rugby to the sports the person is interested in.⚡
+// Expected answer  ✅
+
+// Exercise 3.✍
+
+// 3.1. Return the first set of lucky numbers of the persons set_of_lucky_numbers.⚡
+// Expected answer  ✅
+
+// 3.2. Add the number 15 to the second element of the persons set_of_lucky_numbers.⚡
+// Expected answer  ✅
