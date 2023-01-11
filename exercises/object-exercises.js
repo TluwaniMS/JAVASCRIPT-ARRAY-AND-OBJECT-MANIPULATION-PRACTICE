a4c79ad59c97eb76fee2c3c3e35b63ecf23619f6
tluwaniMS <tluwani2020@gmail.com>
2023-01-11 07:09:04 +0200

    adding-expected-values-to-object-exercises

--- exercises/object-exercises.js
+++ exercises/object-exercises.js
@@ -1,28 +1,28 @@
 // Exercise 1.✍
 
 // 1.1. Return the persons name.⚡
-// Expected answer  ✅
+// Expected answer Themba✅
 
 module.exports.returnThePersonsName = () => {
   const person = { name: "Themba", surname: "Mahlalela", age: 27 };
 };
 
 // 1.2. Return the persons age.⚡
-// Expected answer  ✅
+// Expected answer 27✅
 
 module.exports.returnThePersonsAge = () => {
   const person = { name: "Themba", surname: "Mahlalela", age: 27 };
 };
 
 // 1.3. Return the persons surname.⚡
-// Expected answer  ✅
+// Expected answer Mahlalela ✅
 
 module.exports.returnThePersonsSurname = () => {
   const person = { name: "Themba", surname: "Mahlalela", age: 27 };
 };
 
 // 1.4. Assign a gender property to the person object and assign to it the value 'Female'⚡
-// Expected answer  ✅
+// Expected answer  { name: "Themba", surname: "Mahlalela", age: 27, gender: "Female" }✅
 
 module.exports.addAGenderPropertyToThePerson = () => {
   const person = { name: "Themba", surname: "Mahlalela", age: 27 };
@@ -31,7 +31,7 @@ module.exports.addAGenderPropertyToThePerson = () => {
 // Exercise 2.✍
 
 // 2.1. Return the persons nationality.⚡
-// Expected answer  ✅
+// Expected answer Alienian ✅
 
 module.exports.returnThePersonsNationality = () => {
   const person = {
@@ -46,7 +46,7 @@ module.exports.returnThePersonsNationality = () => {
 };
 
 // 2.2. Return the persons race.⚡
-// Expected answer  ✅
+// Expected answer Alien ✅
 
 module.exports.returnThePersonsRace = () => {
   const person = {
@@ -61,7 +61,7 @@ module.exports.returnThePersonsRace = () => {
 };
 
 // 2.3. Add a property named physical_information with the following object as its value { height:'1.8m',weight:'82KG' }.⚡
-// Expected answer  ✅
+// Expected answer { name: "Lebogang",surname: "Markle",age: 19,physical_information: { height: "1.8m" ,weight: "82KG" }} ✅
 
 module.exports.addThePhysicalInformationPropertyToThePerson = () => {
   const person = {
@@ -74,7 +74,7 @@ module.exports.addThePhysicalInformationPropertyToThePerson = () => {
 // Exercise 3.✍
 
 // 3.1. Return the persons home address street_name.⚡
-// Expected answer  ✅
+// Expected answer "Albertina Sisule" ✅
 
 module.exports.returnThePersonsHomeAddressStreetName = () => {
   const person = {
@@ -89,7 +89,7 @@ module.exports.returnThePersonsHomeAddressStreetName = () => {
 };
 
 // 3.2. Return the persons work address.⚡
-// Expected answer  ✅
+// Expected answer { street_name: "Thabo Mbeko", area_code: 1632 } ✅
 
 module.exports.returnThePersonsWorkAddress = () => {
   const person = {
@@ -104,7 +104,7 @@ module.exports.returnThePersonsWorkAddress = () => {
 };
 
 // 3.3. Return the persons work address area code.⚡
-// Expected answer  ✅
+// Expected answer 1632 ✅
 
 module.exports.returnThePersonsWorkAddressAreaCode = () => {
   const person = {
@@ -119,7 +119,7 @@ module.exports.returnThePersonsWorkAddressAreaCode = () => {
 };
 
 // 3.3. Add a parents property that has properties father and mother, the mother property should be given the value {name:'Engelinah',surname:'Roots'}, and the father property should be given the following value { name:'Elvin',surname:'Roots' }.⚡
-// Expected answer  ✅
+// Expected answer { name: "Ashwin",surname: "Govender",age: 27,parents: { father: { name: "Elvin", surname: "Roots" }, mother: { name: "Engelinah", surname: "Roots" }}} ✅
 
 module.exports.fn = () => {
   const person = {
