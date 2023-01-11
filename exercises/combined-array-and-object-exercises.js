7631d1c5517ba68775557f27292fa556a92c13a3
tluwaniMS <tluwani2020@gmail.com>
2023-01-11 06:28:37 +0200

    adding-expected-values-to-combined-arrays-and-objects

--- exercises/combined-array-and-object-exercises.js
+++ exercises/combined-array-and-object-exercises.js
@@ -1,7 +1,7 @@
 // Exercise 1.✍
 
 // 1.1. Return the persons lucky numbers.⚡
-// Expected answer ✅
+// Expected answer [14, 21, 9] ✅
 
 module.exports.returnThePersonsLuckyNumbers = () => {
   const person = {
@@ -12,7 +12,7 @@ module.exports.returnThePersonsLuckyNumbers = () => {
 };
 
 // 1.2. Return the persons second listed lucky number.⚡
-// Expected answer ✅
+// Expected answer 21 ✅
 
 module.exports.returnThePersonsSecondListedLuckyNumber = () => {
   const person = {
@@ -23,7 +23,7 @@ module.exports.returnThePersonsSecondListedLuckyNumber = () => {
 };
 
 // 1.3. Add the number 5 to the persons lucky numbers.⚡
-// Expected answer ✅
+// Expected answer { name: "Thomas",surname: "Mapheto",lucky_numbers: [14, 21, 9, 5] }✅
 
 module.exports.addNumberFiveTothePersonsLuckyNumbers = () => {
   const person = {
@@ -36,7 +36,7 @@ module.exports.addNumberFiveTothePersonsLuckyNumbers = () => {
 // Exercise 2.✍
 
 // 2.1. Return the total sports the person is interested in.⚡
-// Expected answer ✅
+// Expected answer 3 ✅
 
 module.exports.returnTheTotalSportsThePersonIsInterestedIn = () => {
   const person = {
@@ -49,7 +49,7 @@ module.exports.returnTheTotalSportsThePersonIsInterestedIn = () => {
 };
 
 // 2.2. Add rugby to the sports the person is interested in.⚡
-// Expected answer ✅
+// Expected answer { name: "Leon",surname: "Mapuranga",hobbies: {sports: ["hockey", "swimming", "soccer", "rugby"]} }✅
 
 module.exports.addRugbyToTheSportsThePersonIsInterestedIn = () => {
   const person = {
@@ -64,7 +64,7 @@ module.exports.addRugbyToTheSportsThePersonIsInterestedIn = () => {
 // Exercise 3.✍
 
 // 3.1. Return the first set of lucky numbers of the persons set_of_lucky_numbers.⚡
-// Expected answer ✅
+// Expected answer [2, 9]✅
 
 module.exports.returnTheFirstSetOfThePersonsSetOfLuckyNumbers = () => {
   const person = {
@@ -81,7 +81,7 @@ module.exports.returnTheFirstSetOfThePersonsSetOfLuckyNumbers = () => {
 };
 
 // 3.2. Add the number 15 to the second element of the persons set_of_lucky_numbers.⚡
-// Expected answer ✅
+// Expected answer { name: "Thabang",surname: "Seshoene",beliefs: {set_of_lucky_numbers: [[2, 9],[7, 10, 15],[11, 14]]}}✅
 
 module.exports.addTheNumberFifteenToTheSecondElementOfThePersonsSetOfLuckyNumbers = () => {
   const person = {
